8fce1c0d7e532b9914af40a7db5c5ed93a38863a
Tyler Sommer <sommertm@gmail.com>
2012-09-27 12:34:08 -0700

    Added FileManager for creating file entities

---
+++ Manager/FileManager.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Orkestra\Bundle\ApplicationBundle\Manager;
+
+use Orkestra\Bundle\ApplicationBundle\Entity\File;
+
+class FileManager
+{
+    /**
+     * @var string
+     */
+    protected $basePath;
+
+    /**
+     * Constructor
+     *
+     * @param string $basePath
+     */
+    public function __construct($basePath)
+    {
+        $this->basePath = $basePath;
+    }
+
+    /**
+     * @param string $basePath
+     */
+    public function setBasePath($basePath)
+    {
+        $this->basePath = $basePath;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBasePath()
+    {
+        return $this->basePath;
+    }
+
+    /**
+     * @param resource $gdResource A GD image resource
+     * @param string $filename
+     *
+     * @return \Orkestra\Bundle\ApplicationBundle\Entity\File
+     * @throws \RuntimeException
+     */
+    public function saveImageToPng($gdResource, $filename = null)
+    {
+        if (empty($filename)) {
+            $filename = md5(uniqid('imagepng', true)) . '.png';
+        }
+
+        $fullPath = $this->basePath . '/' . $filename;
+        $result = imagepng($gdResource, $fullPath, 5);
+
+        if (!$result) {
+            throw new \RuntimeException(sprintf('Could not save image to %s', $fullPath));
+        }
+
+        $file = new File($this->basePath, $filename, 'image/png', filesize($fullPath));
+
+        return $file;
+    }
+}
