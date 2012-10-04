925334a4c6fe42bff5dec7ea621cf1563b1abd53
Tyler Sommer <sommertm@gmail.com>
2012-10-04 16:03:26 -0700

    Added EntityChoice form type

---
+++ Form/EntityChoice/ArbitrarySetLoader.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Orkestra\Bundle\ApplicationBundle\Form\EntityChoice;
+
+use Symfony\Bridge\Doctrine\Form\ChoiceList\EntityLoaderInterface;
+use Symfony\Component\Form\Util\PropertyPath;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
+
+class ArbitrarySetLoader implements EntityLoaderInterface
+{
+    protected $entities;
+
+    /**
+     * @param \Closure $setClosure
+     */
+    public function __construct($setClosure)
+    {
+        if ($setClosure instanceof \Closure) {
+            $this->entities = $setClosure();
+        } else {
+            throw new UnexpectedTypeException($setClosure, '\Closure');
+        }
+    }
+
+    /**
+     * Returns an array of entities that are valid choices in the corresponding choice list.
+     *
+     * @return array The entities.
+     */
+    public function getEntities()
+    {
+        return $this->entities;
+    }
+
+    /**
+     * Returns an array of entities matching the given identifiers.
+     *
+     * @param string $identifier The identifier field of the object. This method
+     *                           is not applicable for fields with multiple
+     *                           identifiers.
+     * @param array $values The values of the identifiers.
+     *
+     * @return array The entities.
+     */
+    public function getEntitiesByIds($identifier, array $values)
+    {
+        return array_filter($this->entities->toArray(), function ($entity) use ($identifier, $values) {
+            $propertyPath = new PropertyPath($identifier);
+
+            return in_array($propertyPath->getValue($entity), $values);
+        });
+    }
+}
---
+++ Form/EntityChoiceType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Orkestra\Bundle\ApplicationBundle\Form;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bridge\Doctrine\Form\Type\DoctrineType;
+use Symfony\Bridge\Doctrine\Form\ChoiceList\ORMQueryBuilderLoader;
+
+class EntityChoiceType extends DoctrineType
+{
+    /**
+     * Return the default loader object.
+     *
+     * @param ObjectManager $manager
+     * @param mixed         $queryBuilder
+     * @param string        $class
+     * @return ORMQueryBuilderLoader
+     */
+    public function getLoader(ObjectManager $manager, $queryBuilder, $class)
+    {
+        return new EntityChoice\ArbitrarySetLoader(
+            $queryBuilder
+        );
+    }
+
+    public function getName()
+    {
+        return 'entity_choice';
+    }
+}
