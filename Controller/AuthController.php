5886d74f9d9679fe6ada7079184a9bf29433a359
Tyler Sommer <sommertm@gmail.com>
2014-01-30 15:56:06 -0800

    Updated AuthController

--- Controller/AuthController.php
+++ Controller/AuthController.php
@@ -3,7 +3,7 @@
 namespace Orkestra\Bundle\ApplicationBundle\Controller;
 
 use Symfony\Component\Security\Core\SecurityContext;
-use Symfony\Component\Form\FormError;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
  * User authentication controller
@@ -13,7 +13,7 @@ class AuthController extends Controller
     /**
      * Displays a form to login
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @Template
      */
     public function loginAction()
     {
@@ -23,17 +23,15 @@ class AuthController extends Controller
         $form = $this->createFormBuilder()
             ->add('username', 'text', array('label' => 'Username'))
             ->add('password', 'password', array('label' => 'Password'))
+            ->add('rememberMe', 'checkbox', array('label' => 'Remember Me', 'required' => false))
             ->getForm();
-
-        if ($session->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $form->addError(new FormError('Invalid username or password'));
-            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
-        }
 
         if ($session->has(SecurityContext::LAST_USERNAME)) {
             $form->setData(array('username' => $session->get(SecurityContext::LAST_USERNAME)));
         }
 
-        return $this->render('OrkestraApplicationBundle:Auth:login.html.twig', array('form' => $form->createView()));
+        return array(
+            'form' => $form->createView()
+        );
     }
 }
