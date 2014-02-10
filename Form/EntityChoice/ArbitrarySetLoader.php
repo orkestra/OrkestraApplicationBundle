a4cf0d9483259084518e5f07fcdfc6273ea57d78
Tyler Sommer <sommertm@gmail.com>
2014-02-10 15:55:26 -0800

    Added LICENSE

--- Form/EntityChoice/ArbitrarySetLoader.php
+++ Form/EntityChoice/ArbitrarySetLoader.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the OrkestraApplicationBundle package.
+ *
+ * Copyright (c) Orkestra Community
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
 namespace Orkestra\Bundle\ApplicationBundle\Form\EntityChoice;
 
 use Symfony\Bridge\Doctrine\Form\ChoiceList\EntityLoaderInterface;
